cbeea61d79a285d83567da08055e5521723fcd85
Johann Höchtl <johann.hoechtl@gmail.com>
2017-03-07 10:02:41 +0100

    only render the badge if there is something to display

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -18,16 +18,22 @@ func Log(handler http.Handler) http.Handler {
 func renderbadge(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
-		// Maybe we need to know where we come from: data.gv.at vs. opendataportal.at
+		// Maybe we need to know where we come from
+		// data.gv.at vs. opendataportal.at
 		// and render only for those hosts
+		// Maybe better though to operate that service behind Nginx or like
+		// and perform the port mapping / filtering there
+
 		// host := r.Host
 
 		id := r.URL.Path[1:]
 
-		// do here all the wizardry to retrieve information about this data set
-
-		w.Header().Set("Content-Type", "image/svg+xml")
-		badge.Render("ADEQUATe", id, "#5272B4", w)
+		// for now, do not serve a badge if there is no indication for what ID to retrieve information
+		if len(id) > 0 {
+			// do here all the wizardry to retrieve information about this data set
+			w.Header().Set("Content-Type", "image/svg+xml")
+			badge.Render("ADEQUATe", id, "#5272B4", w)
+		}
 	}
 }
 
